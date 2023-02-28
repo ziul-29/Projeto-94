b167fe9c457a745cad24e599a86bcefdaf5084e4
ziul-29 <108885745+ziul-29@users.noreply.github.com>
2023-02-28 19:40:45 -0300

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBmMvdnkKyKJW-BfVqzjQoJRpWuJV7vwo",
+    authDomain: "projeto-93-1cbf2.firebaseapp.com",
+    databaseURL: "https://projeto-93-1cbf2-default-rtdb.firebaseio.com",
+    projectId: "projeto-93-1cbf2",
+    storageBucket: "projeto-93-1cbf2.appspot.com",
+    messagingSenderId: "308971143632",
+    appId: "1:308971143632:web:881ff920255580de2eb9d5"}
+    const app = initializeApp(firebaseConfig);
